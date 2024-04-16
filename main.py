78c9ad75fc63bea755ad0b55ca39f275ad843999
Quinn Renaghan <quinn@renaghan.org>
2024-04-15 23:57:44 -0400

    update gitignore

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import Board_Utility as util
 import time
 board = set()
 
+start_time = time.time()
+seed = time.time() % 10000
+first_board = util.Board(seed)
 
-for i in range(1000):
-    seed = time.time() % 10000 + i
-    board.add(util.Board(seed))
+for key, value in first_board.tile_dict.items():
+    print(f"\n{key} : {value}")
 
