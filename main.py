0a068645a27230fac10b4beefada99543ffdb542
Quinn Renaghan <quinn@renaghan.org>
2024-04-24 14:59:15 -0400

    timed driver

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import Board_Utility as Util
 import Balanced_Utility as Balanced
 
-best_board = Util.Board()
-best_score = sum(Balanced.balance_score(best_board))
+def get_board(minutes):
+    boards_per_second = 10000.00 / 5
+    seconds = minutes * 60
+    num_boards = int(boards_per_second * seconds)
 
-for num in range(10000):
-    board = Util.Board()
-    score = sum(Balanced.balance_score(board))
-    if score < best_score:
-        best_score = score
-        best_board = board
+    best_board = Util.Board()
+    best_score = sum(Balanced.balance_score(best_board))
 
-print(best_board)
-print(best_score)
+    for num in range(num_boards):
+        board = Util.Board()
+        score = sum(Balanced.balance_score(board))
+        if score < best_score:
+            best_score = score
+            best_board = board
+    return best_board
+
+
+
+
 
 
 
